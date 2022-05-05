93256c21931b1d0748873d8af92e7794853ad594
Davi Santos <102557963+DaviSantosDEV@users.noreply.github.com>
2022-05-05 15:42:14 -0300

    Add files via upload

---
+++ Atividade.js
@@ -0,0 +1,25 @@
+function nome(a){
+    console.log (a)
+    return
+}
+nome('DaviSantos')
+
+function soma(a,b){
+    console.log(a+b)
+    return
+}
+soma(10,10)
+
+function multiplicação(a,b){
+    console.log (a*b)
+    return
+}
+
+multiplicação(9,6)
+
+function media(c,d,x,y){
+    let res = c+d+x+y;
+    console.log(res/4)
+   
+}
+media(8,7,7,5)
